5a2ab6a828434c7ffaa4c1c6a49fbb3c87a53920
Misterdev19 <jcpenagos716@outlook.com>
2022-11-09 22:37:00 -0500

    swalalert

--- views/script/reports.js
+++ views/script/reports.js
@@ -1,21 +1,31 @@
+
+let  url =  '../controllers/reports.php';
 const init = () => {
     $("#btn_upload").on("click" , ()=>{
             upload_data();
       });
 }
+
+
 const upload_data = () =>{
     let frm = document.getElementById("frm_data");
     let data  = new  FormData(frm);
     data.append("accion" , "report");
     $.ajax({
         type:'POST',
-         url:'../controllers/reports.php',
+         url:url,
          data:data,
          processData: false,
          contentType: false,
          success:(rps)=>{
-             console.log(rps);
+              let datos = JSON.parse(rps);
+              if(datos.status){
+                swal("!Atencion!" , datos.msg ,  "success");
+              }else{
+                swal("!Atencion!" , datos.msg ,  "success");
+              }
          }
     });
 }
+
 init();
